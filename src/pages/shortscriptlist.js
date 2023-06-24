cc793b84ba9ac2edcfc38f8f82fc1d9316006956
Bob Slaymaker <bobslaymaker@gmail.com>
2023-06-24 18:08:27 -0400

    l

--- src/pages/shortscriptlist.js
+++ src/pages/shortscriptlist.js
@@ -5,7 +5,7 @@ export default function ShortScripts() {
 
     return (
         <div>
-            <div className="font-serif font-semi bold #000000 background: hex #FDFEFF mt-8 mb-12 ml-8 ms-4 text-2xl tracking-wider leading-relaxed fontFamily: times roman">
+            <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-2xl tracking-wider leading-relaxed ml-12 fontFamily: times roman">
             {shortScripts?.map(shortscript => {
                 return (<div>{shortscript?.content}</div>)
             })}
