4e43e08d4dcbb3dcde4ba16df42e7ec0afc4562a
Bob Slaymaker <bobslaymaker@gmail.com>
2023-06-24 16:56:18 -0400

    l

--- src/pages/shortscripts/[shortscript].js
+++ src/pages/shortscripts/[shortscript].js
@@ -9,7 +9,7 @@ export default function ShortScripts() {
             {shortScripts?.map(shortScript => {
             
                 if (shortScript?.title === router.query.shortscript) {
-                    return <div className="font-georgia #000000 #EFF8FD text-4xl tracking-wider leading-relaxed mt-6 ml-12 mr-12 font-[500]">{shortScript.content}</div>
+                    return <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-2xl tracking-wider leading-relaxed ml-12 fontFamily: times roman">{shortScript.content}</div>
                 }
             })}
         </div>
