6d8012bfbaa92e4ba3dc91b287ea48846d89f2ed
Bob Slaymaker <bobslaymaker@gmail.com>
2023-06-24 14:55:33 -0400

    Removed .eslintrc.json so it doesn't trigger errors during deployment.

--- src/pages/poemlist.js
+++ src/pages/poemlist.js
@@ -5,9 +5,9 @@ export default function PoemList() {
     return (<div className="">
         
         <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-4xl tracking-wider leading-relaxed ml-12">
-            {poems?.map(poem => {
+            {poems?.map((poem, index) => {
                 return (
-                    <Link href={`poems/${poem?.title}`}>{poem?.title}</Link>
+                    <Link href={`poems/${poem?.title}`} key={index}>{poem?.title}</Link>
                 )
             })}
         </div>
