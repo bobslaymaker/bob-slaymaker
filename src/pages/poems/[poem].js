4f745c8788284d39bce4c280900a948aa2526018
Bob Slaymaker <bobslaymaker@gmail.com>
2023-06-24 16:34:50 -0400

    t

--- src/pages/poems/[poem].js
+++ src/pages/poems/[poem].js
@@ -8,7 +8,7 @@ export default function Poem() {
         <div>
             {poems?.map(poem => {
                 if (poem?.title === router.query.poem) {
-                    return <div className="font-georgia #000000 #EFF8FD text-2xl tracking-wider leading-relaxed mt-6 mb-6 ml-12 mr-12 font-[500]">{poem.content}</div>
+                    return <div className="font-times roman #000000 #EFF8FD text-2xl tracking-wider leading-relaxed mt-6 mb-6 ml-12 mr-12 font-[500]">{poem.content}</div>
                 }
             })}
         </div>
