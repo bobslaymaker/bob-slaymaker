aca1850b76cab35c662cb69ad68715488b08c69f
Bob Slaymaker <bobslaymaker@gmail.com>
2023-06-24 16:26:51 -0400

    l

--- src/pages/poemlist.js
+++ src/pages/poemlist.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function PoemList() {
     return (<div className="">
         
-        <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-3xl tracking-wider leading-relaxed ml-12">
+        <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-2xl tracking-wider leading-relaxed ml-12">
             {poems?.map((poem, index) => {
                 return (
                     <Link href={`poems/${poem?.title}`} key={index}>{poem?.title}</Link>
--- src/pages/shortstorieslist.js
+++ src/pages/shortstorieslist.js
@@ -6,7 +6,7 @@ export default function ShortStories() {
 
     return (
         <div>
-            <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-4xl tracking-wider leading-relaxed ml-12">
+            <div className="font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-14 ms-8 text-2xl tracking-wider leading-relaxed ml-12">
             {shortStories?.map(shortStory => {
                 return (
                     <Link href={`shortstories/${shortStory?.title}`}>{shortStory?.title}</Link>
