7d6d60d3ecda893e9f26a4b44bc2aa90c01c49e9
Bob Slaymaker <bobslaymaker@gmail.com>
2023-06-24 21:37:19 -0400

    l

--- src/shortscripts.js
+++ src/shortscripts.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
 export const shortScripts = [
-    {content: <h5 className='font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-12 ms-8 text-2xl tracking-wider leading-relaxed ml-12 fontFamily: times roman'><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 6 8 23.pdf">Neighborhood Women</Link></h5>},
-    {content: <h5 className='font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-12 mb-12 ml-12 ms-8 text-2xl tracking-wider leading-relaxed ml-12 fontFamily: times roman'><Link target="_blank" href="/documents/Zebra Couple.pdf">Zebra Couple</Link></h5>},
+    {content: <h5 className='font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-6 mb-6 ml-6 ms-6 text-2xl tracking-wider leading-relaxed fontFamily: times roman'><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 6 8 23.pdf">Neighborhood Women</Link></h5>},
+    {content: <h5 className='font-serif font-semi bold #000000 background: hex #FDFEFF flex flex-col mt-6 mb-6 ml-6 ms-6 text-2xl tracking-wider leading-relaxed fontFamily: times roman'><Link target="_blank" href="/documents/Zebra Couple.pdf">Zebra Couple</Link></h5>},
 ]
