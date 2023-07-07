109f685ea3c02145dc3f4dd8abad838d344b4230
Bob Slaymaker <bobslaymaker@gmail.com>
2023-07-07 15:55:29 -0400

    l

---
+++ src/pages/ContactInfo.js
@@ -0,0 +1,15 @@
+import {poems} from "../poems"
+import Link from "next/link";
+
+export default function ContactInfo() {
+    return (
+    <div className="goudy">
+
+        
+        <div className="container flex flex-col text-2xl tracking-wider leading-relaxed">
+             <p>bobslaymaker@gmail.com</p><br/>
+             <p>1-646-925-2509</p> 
+        </div>
+        
+    </div>)
+}
