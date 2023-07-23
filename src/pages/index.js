fc3350ac1d07fed875c18dfd13dfecf1aa6e0ac8
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-07-23 01:47:27 -0400

    Added credit to Pichan

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,11 @@ export default function Home() {
 
         </div>
         </div>
-   
+        <div className="goudy pichan">
+          <p>Pichsereyvattana Chan</p>
+          <p>pchan@ithaca.edu</p>
+        </div>
+
       </div>
   );
 }
