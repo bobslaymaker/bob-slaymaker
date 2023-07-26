74849cb830e6804a8df13145f3020374bed8b1b9
Bob Slaymaker <bobslaymaker@gmail.com>
2023-07-26 13:21:54 -0400

    l

--- src/pages/ContactInfo.js
+++ src/pages/ContactInfo.js
@@ -8,7 +8,7 @@ export default function ContactInfo() {
         
         <div className="container flex flex-col text-2xl tracking-wider leading-relaxed">
              <p>Email: <a href="mailto:bobslaymaker@gmail.com">bobslaymaker@gmail.com</a></p>
-             <p>Phone Number: +1-(646)-925-2509</p> 
+             <p>Phone: +1-646-925-2509</p> 
         </div>
         
     </div>)
