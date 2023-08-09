17b5c7a3901982dd70cdbde2d6bf00a155ae0fe1
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-08-09 00:33:48 -0400

    Fixed default icon always showing issues when opening files (*.pdf)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+    // {/* <link rel="icon" href="/bob-slaymaker.jpg" sizes="any" /> */}
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/bob-slaymaker.jpg" sizes="any" />
+    
         <link href="https://fonts.cdnfonts.com/css/goudy-old-style" rel="stylesheet"></link>
       </Head>
       <body>
