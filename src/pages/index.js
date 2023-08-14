6bda90320ce498fe7840f586eee4fc7e1e14e06a
Bob Slaymaker <bobslaymaker@gmail.com>
2023-08-13 22:33:14 -0400

    l

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,10 @@ export default function Home() {
       }}
     >
 {/* 768px */}
-    <div className="goudy text-[18px] md:text-[32px] space-y-5">
+    <div className="goudy text-[18px] md:text-[28px] space-y-5">
         <div className="text-center">
-          <h1 className="font-bold m-5 text-[22px] md:text-[32px]">BOB SLAYMAKER</h1>
-          <p className="font-bold m-5 mb-8 text-[18px] md:text-[32px]">WRITER-FILMMAKER</p>
+          <h1 className="font-bold m-5 text-[22px] md:text-[30px]">BOB SLAYMAKER</h1>
+          <p className="font-bold m-5 mb-8 text-[18px] md:text-[30px]">WRITER-FILMMAKER</p>
           <div className="text-center flex items-center justify-center">
             <hr className="font-bold" style={{border:"1px solid black", width:"200px"}}></hr>
           </div>
@@ -61,7 +61,7 @@ export default function Home() {
           <div className="col-span-6 flex items-start">
             <p className=""></p>
           </div>
-          <div className="col-span-6 text-right">
+          <div className="col-span-6 text-center">
             <p>© 1980-2023 Bob Slaymaker<br/>Website designed by Pichsereyvattana Chan</p>
           </div>
         </div>
