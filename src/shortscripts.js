88372e145e105fe425ff5ebfa21d512de9a308b1
Bob Slaymaker <bobslaymaker@gmail.com>
2023-08-26 12:26:35 -0400

    l

--- src/shortscripts.js
+++ src/shortscripts.js
@@ -4,5 +4,6 @@ export const shortScripts = [
     {content: <h5><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 8 20 23.pdf">Neighborhood Women</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Strangers 8 20 23.pdf">Strangers</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Zebra Couple 8 23 23.pdf">Zebra Couple</Link></h5>},
+    {content: <h5><Link target="_blank" href="/documents/Brothers 8 20 23">Brothers</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/The Applicant Short Script 3 31 13.pdf">The Applicant</Link></h5>},
 ]
