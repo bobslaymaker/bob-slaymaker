02dc947bf35c6592409771ddc509b322172d0c90
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-08-31 23:53:03 -0400

    provided an easier way of changing vertical spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-const inter = Inter({ subsets: ["latin"] });
 
+const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
+  const verticalSpacing = 20
   return (
     <div
-      className={`font-[800] text-center flex  flex-col items-center justify-center p-24 ${inter.className} space-x-5`}
+      className={`font-[800] text-center flex flex-col items-center justify-center p-24 ${inter.className} space-x-5 `}
       style={{
         width: "100vw",
         height: "100vh",
         background: "hex #FDFEFF",
       }}
     >
-{/* 768px */}
-    <div className="goudy text-[18px] md:text-[17px] space-y-5">
-        <div className="text-center">
-          <h1 className="font-bold m-4 text-[18px] md:text-[22px] space-y-1">BOB SLAYMAKER</h1>
-          <p className="font-bold m-4 mb-8 text-[18px] md:text-[22px] space-y-1">WRITER–FILMMAKER</p>
-          <div className="text-center flex items-center justify-center">
-            <hr className="font-bold" style={{border:"1px solid black", width:"200px"}}></hr>
-          </div>
-          
-        </div>
+      {/* 768px */}
+      <div className={`text-center goudy text-[18px] md:text-[17px] space-y-[${verticalSpacing}px]`} style={{ width: "100vw"}}>
         
-        <div className="grid grid-cols md:flex mt-2 mb-2 space-y-5">
-         
-        <div className="font-bold md:m-5">
+        <h1 className="font-bold text-[18px] md:text-[22px] ">BOB SLAYMAKER</h1>
+        <h1 className="font-bold text-[18px] md:text-[22px] ">WRITER–FILMMAKER</h1>
+        <div className="flex items-center justify-center ">
+          <hr className="font-bold text-center" style={{ border: "1px solid black", width: "200px" }}></hr>
+        </div>
+
+        <div className={`grid grid-cols md:flex md:justify-center space-y-[${verticalSpacing}px] `}> 
+          <div className="font-bold md:m-5">
             <Link href="/poemlist">Sample Poems</Link>
           </div>
 
@@ -35,14 +33,11 @@ export default function Home() {
           </div>
 
           <div className="font-bold md:m-5">
-              <Link href="/shortscriptlist">Sample Short Scripts</Link>
+            <Link href="/shortscriptlist">Sample Short Scripts</Link>
           </div>
-        
+
           <div className="font-bold md:m-5">
-            <Link
-              target="_blank"
-              href="https://vimeo.com/manage/videos/118768354/"
-            >
+            <Link target="_blank" href="https://vimeo.com/manage/videos/118768354/">
               My Last Film
             </Link>
           </div>
@@ -54,134 +49,17 @@ export default function Home() {
           <div className="font-bold md:m-5">
             <Link href="/ContactInfo">Contact</Link>
           </div>
-
-
         </div>
-        <div className="grid grid-cols-12 text-[10px] ">
+        <div className="grid grid-cols-12 text-[10px] mt-5">
           <div className="col-span-6 flex items-start">
             <p className=""></p>
           </div>
           <div className="col-span-6 text-center">
-            <p>© 1987–2023 Bob Slaymaker<br/>
-               Website by Pichsereyvattana Chan</p>
+            <p>© 1987–2023 Bob Slaymaker<br />
+              Website by Pichsereyvattana Chan</p>
           </div>
         </div>
-      
       </div>
-
-    
     </div>
   );
 }
-
-/**
- * <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_##f4f5f0] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
- */
