d3b6bef5994e3baf03efa9b18bd1cb8241c58b1a
Bob Slaymaker <bobslaymaker@gmail.com>
2023-09-01 14:48:06 -0400

    l

--- src/shortscripts.js
+++ src/shortscripts.js
@@ -5,5 +5,5 @@ export const shortScripts = [
     {content: <h5><Link target="_blank" href="/documents/Strangers 8 20 23.pdf">Strangers</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Zebra Couple 8 23 23.pdf">Zebra Couple</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Brothers 9 1 23.pdf">Brothers</Link></h5>},
-    {content: <h5><Link target="_blank" href="/documents/The Applicant Short Script 3 31 13.pdf">The Applicant</Link></h5>},
+    {content: <h5><Link target="_blank" href="/documents/The Applicant Short Script 3 31 13.pdf">[The Applicant]</Link></h5>},
 ]
