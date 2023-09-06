f2cf1e7c0b0650f98fb8ad0b1bacc7f995fbae68
Bob Slaymaker <bobslaymaker@gmail.com>
2023-09-06 16:04:36 -0400

    l

--- src/shortscripts.js
+++ src/shortscripts.js
@@ -1,10 +1,9 @@
 import Link from "next/link";
 
 export const shortScripts = [
-    {content: <h5><Link target="_blank" href="/documents/Strangers 8 20 23.pdf">Strangers</Link></h5>},
-    {content: <h5><Link target="_blank" href="/documents/Zebra Couple 8 23 23.pdf">Zebra Couple</Link></h5>},
-    {content: <h5><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 8 20 23.pdf">Neighborhood Women</Link></h5>},
-    {content: <h5><Link target="_blank" href="">Learning the Hard Way [w/identity +/or MPyth-esque additions]</Link></h5>},
+    {content: <h5><Link target="_blank" href="/documents/Strangers 9 5 23.pdf">Strangers</Link></h5>},
+    {content: <h5><Link target="_blank" href="/documents/Zebra Couple 9 5 23.pdf">Zebra Couple</Link></h5>},
+    {content: <h5><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 9 6 23.pdf">Neighborhood Women</Link></h5>},
+    {content: <h5><Link target="_blank" href="">[Learning the Hard Way [w/identity +/or MPyth-esque additions]]</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Brothers 9 1 23.pdf">Brothers</Link></h5>},
-    {content: <h5><Link target="_blank" href="/documents/The Applicant Short Script 3 31 13.pdf">[The Applicant--too valuable to show?]</Link></h5>},
 ]
