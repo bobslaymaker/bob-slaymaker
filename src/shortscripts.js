a684a5b55ec72b1cd7d134a4c9fd4e6cd428e938
Bob Slaymaker <bobslaymaker@gmail.com>
2023-09-21 15:41:04 -0400

    revised neighborhood women

--- src/shortscripts.js
+++ src/shortscripts.js
@@ -3,6 +3,6 @@ import Link from "next/link";
 export const shortScripts = [
     {content: <h5><Link target="_blank" href="/documents/Strangers 9 18 23.pdf">Strangers</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Zebra Couple 9 14 23.pdf">Zebra Couple</Link></h5>},
-    {content: <h5><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 9 15 23.pdf">Neighborhood Women</Link></h5>},
+    {content: <h5><Link target="_blank" href="/documents/NEIGHBORHOOD WOMEN 9 21 23.pdf">Intervention</Link></h5>},
     {content: <h5><Link target="_blank" href="/documents/Brothers 9 19 23.pdf">Brothers</Link></h5>},
 ]
