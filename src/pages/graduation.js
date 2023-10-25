acbee363006283ca20d72b11c196741b6874d48a
Pichsereyvattana Chan <pchan@ithaca.edu>
2023-10-25 13:16:38 -0400

    10/25/2023 - Graduation page added

---
+++ src/pages/graduation.js
@@ -0,0 +1,17 @@
+
+
+
+export default function Graduation() {
+
+
+    return (
+        <div>
+
+            <video autoplay controls>
+            <source src="https://link.storjshare.io/s/jwelrulkhwtmmd5iffh3zvldyrzq/vattana%2FGraduation%20Day.mp4?wrap=0" type="video/mp4"/>
+            Your browser does not support the video tag.
+            </video>
+            {/* <iframe src="https://drive.google.com/file/d/1ZzHT0EjPRXgvcDynN9Iv5dsz5TmfJ_6c/preview" width="640" height="480" allow="autoplay"></iframe> */}
+        </div>
+    )
+}
