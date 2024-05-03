e83725dbfa290a139e952fa6ef78dfef423c0991
Bob Slaymaker <bobslaymaker@gmail.com>
2024-05-03 09:43:58 -0400

    Muneeb 5 3 24

--- src/pages/shortstories/[shortstory].js
+++ src/pages/shortstories/[shortstory].js
@@ -2,11 +2,12 @@ import { useRouter } from "next/router"
 import ShortStories from "../shortstorieslist"
 import { shortStories } from "@/shortstories"
 
+
 export default function shortStory() {
     const router = useRouter()
 
     return (
-        <div className="goudy container text-2xl">
+        <div className="goudy font-size-add mx-auto w-full md:w-[70vw] lg:w-[70vw] 2xl:w-[30vw] text-[24px] md:text-[2.9vw] lg:text-[27px] p-[1em] ">
             {shortStories?.map(shortStory => {
                 if (shortStory?.title === router.query.shortstory) {
                     return <div className="">{shortStory.content}</div>
@@ -16,3 +17,4 @@ export default function shortStory() {
         </div>
     )
 }
+
--- src/pages/shortstorieslist.js
+++ src/pages/shortstorieslist.js
@@ -5,14 +5,14 @@ export default function ShortStories() {
 
 
     return (
-        <div className="goudy">
-            <div className="container text-2xl flex flex-col tracking-wider leading-relaxed">
+        <div className="goudy flex justify-center items-center p-[1em]">
+            <div className="text-2xl flex flex-col tracking-wider leading-relaxed text-[24px] md:text-[2.9vw] lg:text-[27px]">
                 {shortStories?.map(shortStory => {
                     return (
                         <Link href={`shortstories/${shortStory?.title}`}>{shortStory?.title}</Link>
                     )
                 })}
-             </div>
+            </div>
         </div>
     )
 }
