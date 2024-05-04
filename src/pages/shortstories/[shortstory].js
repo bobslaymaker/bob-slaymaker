f27062b1e295ef277c96b0ca32a0448b0de93447
Bob Slaymaker <bobslaymaker@gmail.com>
2024-05-04 09:48:56 -0400

    Fixed Line Length for Laptops on up 5 4 24

--- src/pages/shortstories/[shortstory].js
+++ src/pages/shortstories/[shortstory].js
@@ -2,15 +2,14 @@ import { useRouter } from "next/router"
 import ShortStories from "../shortstorieslist"
 import { shortStories } from "@/shortstories"
 
-
 export default function shortStory() {
     const router = useRouter()
 
     return (
-        <div className="goudy font-size-add mx-auto w-full md:w-[70vw] lg:w-[70vw] 2xl:w-[30vw] text-[24px] md:text-[2.9vw] lg:text-[27px] p-[1em] ">
+        <div className="goudy flex justify-center w-full text-[22px] md:text-[25px] p-[1em] ">
             {shortStories?.map(shortStory => {
                 if (shortStory?.title === router.query.shortstory) {
-                    return <div className="">{shortStory.content}</div>
+                    return <div className="w-full md:w-[680px]">{shortStory.content}</div>
                     
                 }
             })}
