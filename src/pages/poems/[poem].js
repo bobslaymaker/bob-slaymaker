b098ed73d89de11f3581fee4981aa2a9113b661b
Bob Slaymaker <bobslaymaker@gmail.com>
2024-07-08 14:21:37 -0400

    Smaller font poems for phone screen sizes 7 8 24

--- src/pages/poems/[poem].js
+++ src/pages/poems/[poem].js
@@ -7,7 +7,7 @@ export default function Poem() {
 
     return (
         <div className="goudy flex justify-center items-center p-[1em]">
-            <div className="text-2xl flex flex-col tracking-wider leading-relaxed text-[24px] md:text-[2.9vw] lg:text-[27px]">
+            <div className="text-sm flex flex-col tracking-wider leading-relaxed text-[24px] md:text-[2.9vw] lg:text-[27px]">
                 {poems?.map(poem => {
                     if (poem?.title === router.query.poem) {
                         return <div className={`w-full ${poem.label === "Repulsive" ? "md:w-[820px]" : "md:w-[750px]"}`}>
