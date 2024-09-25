d37a8a6801a5713b6a40bac382feeda2df8e52d6
Bob Slaymaker <bobslaymaker@gmail.com>
2024-09-25 15:27:58 -0400

    hamburger adjustment

--- src/components/hamburgerMenu.js
+++ src/components/hamburgerMenu.js
@@ -34,24 +34,24 @@ const HamburgerMenu = () => {
   return (
     <div className="">
       {/* Hamburger Button */}
-      <div className="flex justify-between items-center w-full shadow-md z-50">
+      <div className="flex justify-center items-center w-full shadow-md z-50 relative h-16">
         <button
-          className="p-4 z-50 focus:outline-none bg-white"
+          className="md:p-4 p-1 z-50 focus:outline-none bg-white absolute md:left-5 left-1"
           onClick={toggleMenu}
         >
           {!isOpen ? <Menu size={24} /> : <X size={24} />}
         </button>
-        <div><h1 className="text-[24px] goudy uppercase">Bob Slaymaker</h1></div>
+        <div><h1 className="text-[25px] md:text-[3vw] lg:text-[30px] 2xl:text-[2vw] goudy leading-relaxed tracking-wider py-2 uppercase">Bob Slaymaker</h1></div>
         <div></div>
       </div>
 
       {/* Menu Overlay */}
       <div
         className={`fixed top-0 goudy left-0 shadow-2xl h-screen bg-white transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
-          } md:w-[20%] w-full uppercase font-normal`}
+          } w-full uppercase font-normal`}
       >
-        <div className="flex flex-col justify-center h-full text-center">
-          <div className="flex flex-col items-center justify-center h-full space-y-4 lg:text-[10px] md:text-[10px] text-[10px]">
+        <div className="flex flex-col justify-center h-full text-center pt-20">
+          <div className="flex flex-col items-start justify-between h-full text-[25px] md:text-[3vw] lg:text-[30px] 2xl:text-[2vw] px-16 pb-5">
             <Link href="/shortscriptlist" className="hover:underline">
               Short Scripts
             </Link>
@@ -66,6 +66,9 @@ const HamburgerMenu = () => {
             </Link>
             <Link href="/myLastFilm" className="hover:underline">
               Last Film
+            </Link>
+            <Link href="/" className="hover:underline">
+              Home
             </Link>
             <Link href="/ContactInfo" className="hover:underline">
               Contact
