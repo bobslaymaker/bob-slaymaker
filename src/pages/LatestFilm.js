4c2d0af1d6348608b6a23a5619a8467eb2682c1d
bobslaymaker <bobslaymaker@gmail.com>
2025-01-04 13:19:40 -0500

    Last Film term globally changed to Latest Film

--- src/pages/LatestFilm.js
+++ src/pages/LatestFilm.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Head from 'next/head';
 
-const MyLastFilm = () => {
+const LatestFilm = () => {
   return (
     <div className="flex goudy flex-col items-center h-[90vh] p-4 bg-black">
       <Head>
-        <title>Last Film</title>
+        <title>Latest Film</title>
       </Head>
-      <h6 className="goudy text-[32px] uppercase text-center mt-6 text-white"><b />My Last Film<b /></h6>
+      <h6 className="goudy text-[32px] uppercase text-center mt-6 text-white"><b />Latest Film<b /></h6>
       <div className="relative w-[90%] md:w-[50%] h-[60vw] md:h-[25vw] z-[40] mt-3">
         <video
           src="/Video/GraduationDay.mp4"
@@ -22,4 +22,4 @@ const MyLastFilm = () => {
   );
 };
 
-export default MyLastFilm;
+export default LatestFilm;
