ca76a80bc9f390f8a612327759d9dfd5df36bb54
bobslaymaker <bobslaymaker@gmail.com>
2025-01-08 12:54:36 -0500

    Video accdptable for phone, tablet, laptop, 4k 1 8 25

--- src/pages/LatestFilm.js
+++ src/pages/LatestFilm.js
@@ -3,12 +3,11 @@ import Head from 'next/head';
 
 const LatestFilm = () => {
   return (
-    <div className="flex goudy flex-col items-center h-[90vh] p-4 bg-black">
+    <div className="flex goudy flex-col items-center h-[90vh] bg-black">
       <Head>
         <title>Latest Film</title>
       </Head>
-      <h6 className="goudy text-[32px] uppercase text-center mt-6 text-white"><b />Latest Film<b /></h6>
-      <div className="relative w-[90%] md:w-[50%] h-[60vw] md:h-[25vw] z-[40] mt-3">
+            <div className="relative w-[90%] md:w-[50%] h-[60vw] md:h-[25vw] z-[40] mt-9">
         <video
           src="https://graduationday.s3.us-east-2.amazonaws.com/GraduationDay.mp4"
           className="w-full h-full"
