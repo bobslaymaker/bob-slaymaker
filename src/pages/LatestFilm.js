2e5f0222ae4023275d7af44cc39e9e83f0488219
bobslaymaker <bobslaymaker@gmail.com>
2025-02-24 13:03:45 -0500

    latest film code changes 2 24 25

--- src/pages/LatestFilm.js
+++ src/pages/LatestFilm.js
@@ -1,9 +1,11 @@
 import React, { useState, useRef } from "react";
 import Head from 'next/head';
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 const LatestFilm = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const videoRef = useRef(null);
+  const isMobile = useIsMobile();
 
   const handlePlayPause = () => {
     if (videoRef.current.paused) {
@@ -20,7 +22,7 @@ const LatestFilm = () => {
       <Head>
         <title>Latest Film</title>
       </Head>
-      <div className="relative w-[90%] md:w-[50%] landscape:h-[calc(100vh-120px)] h-[60vw] md:h-[25vw] z-[40] mt-9">
+      <div className={`relative w-[90%] md:w-[50%] ${isMobile && 'landscape:h-[calc(100vh-110px)]' } h-[60vw] md:h-[25vw] z-[40] mt-9`}>
         <video
           ref={videoRef}
           src="https://graduationday.s3.us-east-2.amazonaws.com/GraduationDay.mp4"
